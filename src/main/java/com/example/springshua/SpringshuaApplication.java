95be16d4aadff56458f4b0973749f199da484f9f
LeslieShua <southenm@gmail.com>
2023-01-18 13:56:03 +0900

    Initial commit

---
+++ src/main/java/com/example/springshua/SpringshuaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springshua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringshuaApplication.class, args);
+	}
+
+}
